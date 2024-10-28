2a00ca8ff47e99a0f6720ae3e8d6432f95185eab
Tommaso Parigi <tommaso.parigi06@gmail.com>
2024-10-28 12:22:01 +0100

    Fix

--- src/main/java/it/itismeucci/Main.java
+++ src/main/java/it/itismeucci/Main.java
@@ -3,12 +3,10 @@ package it.itismeucci;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         ServerSocket ss = new ServerSocket(3000);
-        ArrayList<String> users = new ArrayList<String>();
         while (true) {
             Socket s = ss.accept();
             MyThread t = new MyThread(s);
